5162980153f0f5f6793e51bf3bf26b63400a5e9f
Young Liu <youngliu92@gmail.com>
2019-05-08 22:46:03 -0700

    add ability to save tickets to firebase.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,16 @@
 import constants from "./../constants";
 const { firebaseConfig } = constants;
-import firebase from "firebase";
+import Firebase from "firebase";
 
 firebase.initializeApp(firebaseConfig);
+const tickets = firebase.database().ref("tickets");
+
+export function addTicket(_names, _location, _issue) {
+  return () =>
+    tickets.push({
+      names: _names,
+      location: _location,
+      issue: _issue,
+      timeOpen: new Date().getTime()
+    });
+}
